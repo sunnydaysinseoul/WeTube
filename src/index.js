e180e670eaacc9b24f54b2c701920af15c7d4216
Youngsun Han <youngsunhan.kr@gmail.com>
2022-09-29 16:25:03 +0900

    first get request

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// import express from "express";
-
-/*  //import하지 않는 방식
-const express = require("express");
-const app = express();
-*/
-
-
-console.log("I'm good");
--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
-// import express from "express";
+import express from "express";
+/*
+-화살표 함수 기본 문법
+var/let/const 함수명 = (매개변수) => {실행문}
+-이건 다음과 같음..
+function 함수명(매개변수){
+    실행문;
+}
+*/ 
 
-/*  //import하지 않는 방식
-const express = require("express");
+//변수지정
+const PORT = 4000;
+
+//Express Application(서버) 생성하기
 const app = express();
-*/
+
+//Application 설정하기 ("URL",function))
+app.get("/",() =>console.log("Get request : ROOT에 접속"));
 
 
-console.log("I'm good");
+//Port를열고 외부 접속(request)을 listen하기
+const handleListening = () => console.log(`<Server listening on port ${PORT}.👍\nCheck out http://localhost:${PORT} !>`);
+app.listen(4000,handleListening);

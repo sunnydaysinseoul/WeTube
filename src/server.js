398b0bf85509e971b7abc7179c081992c30eb8ef
Youngsun Han <youngsunhan.kr@gmail.com>
2022-10-21 13:34:59 +0900

    add get request to /home

--- src/server.js
+++ src/server.js
@@ -14,10 +14,19 @@ const PORT = 4000;
 //Express Application(서버) 생성하기
 const app = express();
 
+//Port를열고 외부 접속(request)을 listen하기
+const handleListening = () => 
+    console.log(`<Server listening on port ${PORT}.👍\nCheck out http://localhost:${PORT} !>`);
+app.listen(4000,handleListening);
+
 //Application 설정하기 ("URL",function))
 app.get("/",() =>console.log("Get request : ROOT에 접속"));
 
 
-//Port를열고 외부 접속(request)을 listen하기
-const handleListening = () => console.log(`<Server listening on port ${PORT}.👍\nCheck out http://localhost:${PORT} !>`);
-app.listen(4000,handleListening);
+//localhost:4040/home 과  request주고받기
+const handleHome = (req,res)=> {
+    return res.end();
+};
+
+
+app.get("/home",handleHome);

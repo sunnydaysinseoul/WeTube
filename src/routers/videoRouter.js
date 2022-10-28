1670cd00c14cec5450c441fb3a24bdd50c9e05ae
Youngsun <youngsunhan.kr@gmail.com>
2022-10-28 17:46:52 +0900

    router순서변경, 위에서부터 읽기때문에 :id값이 일반 route보다 위에 있으면 route인식에 문제.

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,8 @@ import { deleteVideo, editVideo, uploadVideo, watchVideo } from "../controllers/
 
 const videoRouter = express.Router();
 
+videoRouter.get("/upload",uploadVideo);
 videoRouter.get("/:vId/watch",watchVideo);
 videoRouter.get("/:vId/edit",editVideo);
 videoRouter.get("/:vId/delete",deleteVideo);
-videoRouter.get("/upload",uploadVideo);
 export default videoRouter; //다른 파일에서 video globalRouter를 하면, 이렇게 export default한 변수를 사용하게 됨.

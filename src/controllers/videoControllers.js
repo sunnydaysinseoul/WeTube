6baf46294e846866f07f05ba26ddeae57ab4bd09
Youngsun <youngsunhan.kr@gmail.com>
2022-11-02 13:23:45 +0900

    Post요청 보내보기

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -35,14 +35,20 @@ export const watchVideo = (req,res)=> {
     const { vId } = req.params; //이 줄은 위에줄과 완전히 같은 뜻.
     const video = videosArr[vId-1]; //id가 1부터 시작하니까..
     console.log(vId);
-    return res.render("watch",{pageTitle:`Watching ${video.title}`,video});
+    return res.render("watch",{pageTitle:`Watching: ${video.title}`,video});
 }
 
-/* URL : /videos/:vId/edit */
+/* URL : (GET) /videos/:vId/edit */
 export const editVideo = (req,res)=> {
-    return res.render("edit");
+    const { vId } = req.params; 
+    const video = videosArr[vId-1];
+    return res.render("edit",{pageTitle:`Editing: ${video.title}`,video});
 }
 
+/* URL : (POST) /videos/:vId/edit */
+export const editVideoSave = (req,res)=> {
+  
+}
 /* URL : /videos/:vId/delete */
 export const deleteVideo = (req,res)=> {
     return res.send("Delete videos.");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { deleteVideo, editVideo, uploadVideo, watchVideo } from "../controllers/videoControllers.js";
+import { deleteVideo, editVideo, uploadVideo, watchVideo, editVideoSave } from "../controllers/videoControllers.js";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload",uploadVideo);
 videoRouter.get("/:vId(\\d+)",watchVideo); //regex. vId로 숫자만 올수있다는뜻
 videoRouter.get("/:vId/edit",editVideo);
+videoRouter.post("/:vId/edit",editVideoSave);
 videoRouter.get("/:vId/delete",deleteVideo);
 export default videoRouter; //다른 파일에서 video globalRouter를 하면, 이렇게 export default한 변수를 사용하게 됨.

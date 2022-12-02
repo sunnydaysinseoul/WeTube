9fe34a36c37bf4ed30167367e1881ca1f95c895f
Youngsun Han <youngsunhan.kr@gmail.com>
2022-12-02 22:15:03 +0900

    upload scss 커밋빠져서 다시만듦...

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,22 +1,24 @@
 import "../scss/style.scss";
 const videoInput = document.getElementById("video");
 
-export const handlevideoInput = (event) =>{
-    // console.log("이벤트발생");
-    document.getElementById("videoLoading").style.visibility = "visible";
-    const reader = new FileReader();
+export const handleVideoClick = (event) => {
+  document.getElementById("videoLoading").style.visibility = "visible";
+};
+export const handleVideoInput = (event) => {
+  const reader = new FileReader();
   reader.onload = () => {
-    // console.log('파일 업로드 완료.');
     document.getElementById("videoLoading").style.visibility = "hidden";
-    console.log(event.target.files[0].name);
+    // console.log(event.target.files[0].name);
   };
-  reader.readAsText(event.target.files[0]);
-}
-export const handleLoading = () =>{
-    document.getElementById("page").style.display = "block";
-    document.getElementById("loading").style.display = "none";
-}
+  document.getElementById("videoLoading").style.visibility = "hidden";
+};
+export const handleLoading = () => {
+  document.getElementById("page").style.display = "block";
+  document.getElementById("loading").style.display = "none";
+};
 window.addEventListener("load", handleLoading);
-if(videoInput){
-videoInput.addEventListener("input",handlevideoInput);
+
+if (videoInput) {
+  videoInput.addEventListener("click", handleVideoClick);
+  videoInput.addEventListener("input", handleVideoInput);
 }

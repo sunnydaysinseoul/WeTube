f8c4fa3847a7a9ff76503adf8069f27e50d9f84a
Youngsun Han <youngsunhan.kr@gmail.com>
2022-12-04 20:39:51 +0900

    db.js 수정

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.DB_URL,{useNewUrlParser:true,useUnifiedTopology:tru
 const db = mongoose.connection;
 
 const handleOpen=()=>console.log("✅Connected to DB.");
-const handleError=(error)=>console.log("❌DB connection Error!");
+const handleError=(error)=>console.log("❌DB connection Error!"+error);
 
 db.on("error",handleError); //error가 발생할 떄 마다
 db.once("open",handleOpen); //DB가 1번(once) 오픈되면
